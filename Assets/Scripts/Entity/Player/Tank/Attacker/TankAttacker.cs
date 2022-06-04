2c9022a6b064f5209b926bb71bc956d7b4f6a4b7
gkaragoz <gkaragoz96@gmail.com>
2022-06-04 21:28:07 +0300

    Add cooldown to attacker.

--- Assets/Scripts/Entity/Player/Tank/Attacker/TankAttacker.cs
+++ Assets/Scripts/Entity/Player/Tank/Attacker/TankAttacker.cs
@@ -8,10 +8,26 @@ namespace Entity.Player.Tank.Attacker
         [SerializeField] private Transform _gunEndPoint;
         [SerializeField] private GameObject _projectilePrefab;
 
+        [SerializeField] private float _cooldown = 0.25f;
+
+        private float _lastAttackedTime = 0;
+
         public Action OnAttackCallback { get; set; }
+
+        private bool IsReadyToNextAttack()
+        {
+            return Time.time >= _lastAttackedTime + _cooldown;
+        }
+        
         public bool Attack()
         {
-            return true;
+            if (IsReadyToNextAttack())
+            {
+                _lastAttackedTime = Time.time;
+                return true;
+            }
+            
+            return false;
         }
 
         public GameObject GetProjectilePrefab()
